530ec0c319dd1ed14acd7a051552fc5bf71bb178
ilaksono <ian.laksono@gmail.com>
2020-10-05 22:25:24 +0000

    add css color change

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,6 +5,8 @@ $(document).ready(function () {
     
     setTimeout(() => {
       let length = $('#tweet-text').val().length;
+      if(length > 140) $('[name = "counter"]').css('color', 'red');
+      else $('[name = "counter"]').css('color', 'black');
       $('[name = "counter"]').text(`${140 - length}`);
     }
       , 0);
