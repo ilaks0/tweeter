d7e9662c099f018b2252cbe96dea79288d817e27
ilaksono <ian.laksono@gmail.com>
2020-10-06 17:50:29 +0000

    add JQ append tweet function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,46 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function () {
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  };
+
+  const createTweetElement = (tweetData) => {
+    const newName = $('<span>').text(tweetData.user.name);
+    const newHandle = $('<span>').text(tweetData.user.handle);
+    const newHeader = $('<header>').append(newName).append(newHandle);
+    const newContent = $('<div>').text(tweetData.content.text);
+    const date = $('<span>').text((new Date(tweetData['created_at'])).toUTCString());
+    const like = $('<span>').text('Like');
+    const newFooter = $('<footer>').append(date).append(like);
+    const $newArticle = $('<article>').append(newHeader).append(newContent).append(newFooter);
+    return $newArticle;
+  };
+  const renderTweets = (tweetsArr) => {
+    for(const element of tweetsArr) {
+      const $tweet = createTweetElement(element);
+      $('#tweets-container').append($tweet);
+    }
+  }
+
+
+
+
+  const $tweet = createTweetElement(tweetData);
+
+  // Test / driver code (temporary)
+  console.log($tweet); // to see what it looks like
+  $('#tweets-container').append($tweet);
+
+
+});
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,10 @@
 $(document).ready(function () {
 
-  $('#tweet-text').on('keydown', () => {
-    setTimeout(() => {
-      let length = $('#tweet-text').val().length;
-      if (length > 140) $('.counter').css('color', 'red').css('font-weight', 'bold');
-      else $('.counter').css('color', 'black').css('font-weight', 'normal');
-      $('.counter').text(`${140 - length}`);
-    }, 0);
+  $('#tweet-text').on('input', () => {
+    let length = $('#tweet-text').val().length;
+    if (length > 140) $('.counter').css('color', 'red').css('font-weight', 'bold');
+    else $('.counter').css('color', 'black').css('font-weight', 'normal');
+    $('.counter').text(`${140 - length}`);
   });
 
 });
