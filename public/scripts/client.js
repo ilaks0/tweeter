af9147490de8c955d41e9f31aa1c9650d9e8a296
ilaksono <ian.laksono@gmail.com>
2020-10-06 22:49:31 +0000

    format spacing

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -9,17 +9,17 @@ $(document).ready(function () {
       renderTweets(tweets);
     });
   const createTweetElement = (tweetData) => {
-    const newAva = $('<img>').attr('src', tweetData.user.avatars);
-    const imgDiv = $('<span>').append(newAva);
-    const nameDiv = $('<span>').text(tweetData.user.name);
-    const newName = $('<span>').append(imgDiv).append(nameDiv);
-    const newHandle = $('<span>').text(tweetData.user.handle);
-    const newHeader = $('<header>').append(newName).append(newHandle);
-    const newContent = $('<div>').text(tweetData.content.text);
-    const date = $('<span>').text((new Date(tweetData['created_at'])).toUTCString());
-    const like = $('<span>').text('Like');
-    const newFooter = $('<footer>').append(date).append(like);
-    const $newArticle = $('<article>').append(newHeader).append(newContent).append(newFooter);
+    const $newAva = $('<img>').attr('src', tweetData.user.avatars);
+    const $imgDiv = $('<span>').append($newAva);
+    const $nameDiv = $('<span>').text(tweetData.user.name);
+    const $newName = $('<span>').append($imgDiv).append($nameDiv);
+    const $newHandle = $('<span>').text(tweetData.user.handle);
+    const $newHeader = $('<header>').append($newName).append($newHandle);
+    const $newContent = $('<div>').text(tweetData.content.text);
+    const $date = $('<span>').text((new Date(tweetData['created_at'])).toUTCString());
+    const $like = $('<span>').text('Like');
+    const $newFooter = $('<footer>').append($date).append($like);
+    const $newArticle = $('<article>').append($newHeader).append($newContent).append($newFooter);
     return $newArticle;
   };
 
@@ -34,9 +34,6 @@ $(document).ready(function () {
   $('form').on('submit', function () {
     event.preventDefault();
     $('.error').hide();
-    // if ($(this).serialize().length > 145) throw 'Post cannot be longer than 140 characters';
-    // if ()
-    // console.log($(this).serialize().length);
     $.ajax({ method: 'POST', url: '/tweets', data: $(this).serialize() })
       .done(function (msg) {
         $.ajax('/tweets', { method: 'GET' })
@@ -48,19 +45,12 @@ $(document).ready(function () {
         // error handling
         $('#tweet-text').addClass('c-error');
         $('.error').text(xhr.responseJSON.error);
-        if ($(".error").first().is(":hidden")) {
-          $(".error").slideDown("slow");
-        }
+        if ($(".error").first().is(":hidden")) $(".error").slideDown("slow");
       });
-
   });
   $('nav > a').click(() => {
-    if ($("form").first().is(":hidden")) {
-      $("form").slideDown("slow");
-    }
+    if ($("form").first().is(":hidden")) $("form").slideDown("slow");
     else $("form").slideUp('slow');
   });
-
-
 });
 
