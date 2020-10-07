77fe5e08d3570eaf21796918d13fbf733ea70dc8
ilaksono <60628041+ilaksono@users.noreply.github.com>
2020-10-07 19:30:24 -0400

    add spacing and refactor

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,24 +1,25 @@
 $(document).ready(function () {
   $.ajax("/tweets", { method: "GET" }).then((tweets) => renderTweets(tweets));
 
-  const createTweetElement = (tweetData) => { // construct each object and tag with text data
+  const createTweetElement = (tweetData) => {
+    // construct each object and tag with text data
     const $newAva = $("<img>").attr("src", tweetData.user.avatars);
     const $imgDiv = $("<span>").append($newAva); // append each tag to its parent
     const $nameDiv = $("<span>").text(tweetData.user.name);
     const $newName = $("<span>").append($imgDiv).append($nameDiv);
     const $newHandle = $("<span>").text(tweetData.user.handle);
     const $newHeader = $("<header>").append($newName).append($newHandle);
-    
+
     const $newContent = $("<body>").text(tweetData.content.text);
     const timeDiff = timeSincePost(tweetData["created_at"]); // calculate time difference since post creation
-    const $date = $("<span>").text(timeDiff); 
+    const $date = $("<span>").text(timeDiff);
     const $flag = $("<img>").attr("src", "./images/flag.png");
     const $retweet = $("<img>").attr("src", "./images/retweet.png");
     const $heart = $("<img>").attr("src", "./images/heart.png");
-    
+
     const $like = $("<div>").append($flag).append($retweet).append($heart);
     const $newFooter = $("<footer>").append($date).append($like);
-    const $newArticle = $("<article>") 
+    const $newArticle = $("<article>")
       .append($newHeader) // append all elements to article parent
       .append($newContent)
       .append($newFooter);
@@ -30,7 +31,8 @@ $(document).ready(function () {
     $("#tweets-container").empty(); // empty tweet section to prevent duplication
     for (const element of tweetsArr) {
       const $tweet = createTweetElement(element);
-      $($tweet).hover( // add hover event handler to each article
+      $($tweet).hover(
+        // add hover event handler to each article
         function () {
           $("footer > div", this).addClass("hover-div"); // on hover, footer icons become visible
           $("header img", this).addClass("hover-div"); // avatar img more visible
@@ -55,7 +57,7 @@ $(document).ready(function () {
         $(".counter").text(140); // reset and show char count after submitting
         $("#tweet-text").val(""); // empty text area
       })
-      .fail(xhr => {
+      .fail((xhr) => {
         // error handling
         $("#tweet-text").addClass("c-error");
         $(".error").text(xhr.responseJSON.error);
@@ -65,7 +67,8 @@ $(document).ready(function () {
       });
   });
 
-  $("nav > button").click(() => { // compose button in nav - toggle form's hideability
+  $("nav > button").click(() => {
+    // compose button in nav - toggle form's hideability
     if ($("form").first().is(":hidden")) {
       $("form").slideDown("fast", () => {
         $("#tweet-text").focus();
@@ -73,7 +76,8 @@ $(document).ready(function () {
     } else $("form").slideUp("fast");
   });
 
-  const timeSincePost = (dateEpoch) => { // calculate time difference since post creation
+  const timeSincePost = (dateEpoch) => {
+    // calculate time difference since post creation
     let unit = "second";
     let diff = (new Date().getTime() - dateEpoch) / 1000;
 
