c1fa128f723871d27f4c2f6da26358d7a1aefe12
ilaksono <ian.laksono@gmail.com>
2020-10-07 00:56:19 +0000

    add hover transition on toggle button

--- public/scripts/composer-anchor-toggle.js
+++ public/scripts/composer-anchor-toggle.js
@@ -1,6 +1,21 @@
 $(document).ready(() => {
+  const $scrollBtn = $('.anchor-toggle');
+  $scrollBtn.hide();
+  $(document).scroll(() => scrollFunction());
+  const scrollFunction = () => {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      // $scrollBtn.removeClass('btn-hide');
+      $scrollBtn.fadeIn('fast');
+    } else {
+      $scrollBtn.hide();
+      // $scrollBtn.addClass('btn-hide');
+    }
+  };
+  $('.anchor-toggle').click(() => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  });
 
-  
 
 
 });
