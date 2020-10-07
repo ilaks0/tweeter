ae61f0d6f86f6a8ed12d4d7a8e304b81855889f0
ilaksono <60628041+ilaksono@users.noreply.github.com>
2020-10-07 15:32:16 -0400

    fix smooth scroll toggle

--- public/scripts/composer-anchor-toggle.js
+++ public/scripts/composer-anchor-toggle.js
@@ -5,35 +5,33 @@ $(document).ready(() => {
     if (
       document.body.scrollTop > 180 ||
       document.documentElement.scrollTop > 180
-      ) {
-        $("nav > button").hide();
-        $scrollBtn.show();
-      } else {
-        $scrollBtn.hide();
-        $("nav > button").show();
-      }
-    };
-    $scrollBtn.click(() => {
-    document.body.scrollTop = 120;
-    document.documentElement.scrollTop = 120;
-    
-    if(!$('form').is(':hidden')) {
+    ) {
+      $("nav > button").hide();
+      $scrollBtn.show();
+    } else {
+      $scrollBtn.hide();
+      $("nav > button").show();
+    }
+  };
+  $scrollBtn.click(() => {
+    if (!$("form").is(":hidden")) {
+      document.body.scrollTop = 120;
+      document.documentElement.scrollTop = 120;
       setTimeout(() => {
         $scrollBtn.hide();
-        $('nav > button').show();
-      }, 700)
-      
+        $("nav > button").show();
+      }, 500);
       $("#tweet-text").focus();
-    }
-    else {
+    } else {
+      document.body.scrollTop = 50;
+      document.documentElement.scrollTop = 50;
       $("form").show(() => {
-      $scrollBtn.hide();
-      $("#tweet-text").focus();
-      $('nav > button').show();
-    });
+        $scrollBtn.hide();
+        $("#tweet-text").focus();
+        $("nav > button").show();
+      });
     }
   });
-  
-  $(document).scroll(() => scrollFunction());
 
+  $(document).scroll(() => scrollFunction());
 });
