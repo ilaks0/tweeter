de63114dd4bbcd0ac766f1ee7faf746ca3110bcb
ilaksono <60628041+ilaksono@users.noreply.github.com>
2020-10-07 13:01:02 -0400

    add button toggle hide on click

--- public/scripts/composer-anchor-toggle.js
+++ public/scripts/composer-anchor-toggle.js
@@ -1,21 +1,39 @@
 $(document).ready(() => {
   const $scrollBtn = $(".anchor-toggle");
   $scrollBtn.hide();
-  $(document).scroll(() => scrollFunction());
   const scrollFunction = () => {
-    if (document.body.scrollTop > 200 ||
-      document.documentElement.scrollTop > 200) {
-      $('nav > a').hide(1000);
-      $scrollBtn.fadeIn("fast");
-    } else {
-      $scrollBtn.hide();
-      $('nav > a').show();
+    if (
+      document.body.scrollTop > 180 ||
+      document.documentElement.scrollTop > 180
+      ) {
+        $("nav > a").hide();
+        $scrollBtn.fadeIn("fast");
+      } else {
+        $scrollBtn.hide();
+        $("nav > a").show();
+      }
+    };
+    $scrollBtn.click(() => {
+    document.body.scrollTop = 120;
+    document.documentElement.scrollTop = 120;
+    
+    if(!$('form').is(':hidden')) {
+      setTimeout(() => {
+        $scrollBtn.hide();
+        $('nav > a').show();
+      }, 700)
+      
+      $("#tweet-text").focus();
     }
-  };
-  $scrollBtn.click(() => {
-    $("form").show(1200);
-    document.body.scrollTop = 180;
-    document.documentElement.scrollTop = 180;
-    $("#tweet-text").focus();
+    else {
+      $("form").show(() => {
+      $scrollBtn.hide();
+      $("#tweet-text").focus();
+      $('nav > a').show();
+    });
+    }
   });
+  
+  $(document).scroll(() => scrollFunction());
+
 });
